b0ef1ab499460ff195a2e3ffcedef8df9d76299e
Ariel Sobocki <ariel.sobocki.84@gmail.com>
2023-04-21 18:15:22 -0300

    Arreglo de la app: valor ingresado es numerico

--- ej1.c
+++ ej1.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-int i, numero[4];
+int i, num, numero[4];
 char valores[4] = {'k', 'n', 'r', 'y'};
 int main(){
     for (i = 0; i < 4; i++)/*iteramos el vector numeros y valores*/
     {
-        printf("Ingresar un valor numerico para la variable %c: \n", valores[i]);
-        scanf("%d", &numero[i]);
+        printf("Ingresar un valor numerico para la variable %c: \n", valores[i]);//se pide por consola los valores numericos
+        while (scanf("%d", &numero[i]) != 1)/*Si el valor ingresado no es numerico*/
+        {
+            scanf("%d", &numero[i]);
+            printf("Error! Ingrese solo valores numericos. \nIngresar un valor numerico para la variable %c: \n", valores[i]);
+            while (getchar() != '\n') {}/*Limpiar el buffer de entrada*/
+            continue;
+        }
         while (numero[2] == 3)/*Si ingresa un 3 para la variable r, se pide que ingrese otro valor*/
         {
-            printf("ERROR!! \nEl numero r debe ser diferente a %d.\n", numero[2]);
+            printf("Error! \nEl numero r debe ser diferente a %d.\n", numero[2]);
             printf("Ingresar valor de %c otra vez.\n", valores[2]);
             scanf("%d", &numero[2]);
             continue;
         }
         while (numero[1]  >= numero[0])/*La variable k debe ser mayor que la variable n*/
         {
-            printf("ERROR!! \nLa variable %c debe ser menor a la %c.\n", valores[1], valores[0]);
+            printf("Error! \nLa variable %c debe ser menor a la %c.\n", valores[1], valores[0]);
             printf("Ingresar valor de %c otra vez.\n", valores[1]);
             scanf("%d", &numero[1]);
             continue;
